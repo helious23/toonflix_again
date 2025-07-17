9c4373cb7387be3e8e11cda3c753d10d17fc6719
Max16 <max16@naver.com>
2025-07-17 16:01:51 +0900

    #6.12 ApiService

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class DetailScreen extends StatelessWidget {
   final WebtoonModel webtoon;
@@ -24,29 +25,70 @@ class DetailScreen extends StatelessWidget {
         shadowColor: Colors.black,
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: FutureBuilder(
+        future: ApiService.getToonById(webtoon.id),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData) {
+            return const Center(child: Text("No Data Found"));
+          }
+
+          return Column(
             children: [
-              Hero(
-                tag: webtoon.id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: webtoon.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withValues(
+                              alpha: 0.3,
+                            ),
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: Image.network(
+                        webtoon.thumb,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                  child: Image.network(
-                    webtoon.thumb,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              Text(
+                snapshot.data!.title,
+                style: const TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                snapshot.data!.about,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                snapshot.data!.genre,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                snapshot.data!.age,
+                style: const TextStyle(fontSize: 16),
               ),
             ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
